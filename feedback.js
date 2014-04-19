3d8add77ab2f30df66ef7411a6ec3f920c3caf51
hardikj <hardikjuneja.hj@gmail.com>
2014-04-19 20:11:14 +0530

    adding routes and basic logic for app

--- feedback.js
+++ feedback.js
@@ -1,33 +1,69 @@
-Messages = new Meteor.Collection('messages');
+/*
+
+*/
+
+/*
+===============
+ Routes
+==============
+*/
+
+Router.configure({
+  layoutTemplate: 'layout',
+  notFoundTemplate: 'notFound',
+  loadingTemplate: 'loading'
+});
+
+// route for main screen
+Router.map(function () {
+  this.route('dashboard');
+});
+
+// rooute for clients
+Router.map(function () {
+  this.route('entryfield', {path:'/'});
+})
+
+/*
+==============
+  Models
+==============
+*/
+
+Feedback = new Meteor.Collection('feedback');
+
+
+/*
+====================
+  Application logic
+====================
+*/
 
 Meteor.startup(function () {
-
-    // client startup code here 
-    Messages.remove ({})
+  // client startup code here 
+  Feedback.remove ({})
 });
 
 
 if (Meteor.isClient) {
-  Template.messages.messages = function(){
-    return Messages.find({}, { sort: { time: -1 }});
+  
+  Template.dashboard.messages = function(){
+    return Feedback.find({}, { sort: { time: -1 }});
   }
 
   Template.entryfield.events = {
-   "keydown #message": function(event){
+   "keydown #feedback": function(event){
     if(event.which == 13){
       // Submit the form
-      var name = document.getElementById('name');
-      var message = document.getElementById('message');
+      var feedback = document.getElementById('feedback');
 
-      if(name.value != '' && message.value != ''){
-        Messages.insert({
-          name: name.value,
-          message: message.value,
+      if(feedback.value != ''){
+        Feedback.insert({
+          name: feedback.value,
           time: Date.now()
         });
 
         name.value = '';
-        message.value = '';
       }
     }
    }
